c9bd4ffc25ade7545439d31ac874828adcfe5621
Victor Maciel Ghost <vtbolado17@gmail.com>
2024-02-25 12:50:51 -0300

    Global datas and procedures

--- source/process/data.php
+++ source/process/data.php
@@ -4,61 +4,7 @@
         By Victor Ghost - 18/12/23 
 
     */
-    
-//========================================//
-
-                                     
-    #####    ##   ##### #    #  ####  
-    #    #  #  #    #   #    # #      
-    #    # #    #   #   ######  ####  
-    #####  ######   #   #    #      # 
-    #      #    #   #   #    # #    # 
-    #      #    #   #   #    #  ####   
-
-//========================================//
-
-    // Website paths
-    const REDIRECT_PAGE_URLS = [
-        "/login/",
-        "/login/register/",
-        "/ucp/",
-        "/installer/"
-    ];
-
 //===================================================================================//
-                                                                               
-    #     #                                                                         
-    #     #  ####  ###### #####     #####  ###### #        ##   ##### ###### #####  
-    #     # #      #      #    #    #    # #      #       #  #    #   #      #    # 
-    #     #  ####  #####  #    #    #    # #####  #      #    #   #   #####  #    # 
-    #     #      # #      #####     #####  #      #      ######   #   #      #    # 
-    #     # #    # #      #   #     #   #  #      #      #    #   #   #      #    # 
-     #####   ####  ###### #    #    #    # ###### ###### #    #   #   ###### #####    
-
-//===================================================================================// 
-    // Enumeration the paths
-
-    const URL_PAGE_LOGIN        =   0;
-    const URL_PAGE_REGISTER     =   1;
-    const URL_PAGE_UCP          =   2;
-    const URL_PAGE_INSTALLER    =   3;
-
-    // Users SESSION variables
-
-    const USER_DATA_ID              =   "UserID";
-    const USER_DATA_NAME            =   "UserName";
-    const USER_DATA_LOGGED          =   "Logged";
-    const USER_DATA_ACCOUNT_TYPE    =   "AccountType";
-
-    // User Account types
-
-    const ACCOUNT_TYPE_ANY          =   0;    // Any
-    const ACCOUNT_TYPE_ADMIN        =   1;    // Pastor
-    const ACCOUNT_TYPE_LEAD_MEMBER  =   2;    // Lead of cell group
-    const ACCOUNT_TYPE_MEMBER       =   3;    // Member of cell group
-
-//===================================================================================//
-
     ######                                                  
     #     #   ##   #####   ##   #####    ##    ####  ###### 
     #     #  #  #    #    #  #  #    #  #  #  #      #      
@@ -73,5 +19,85 @@
     const DATABASE_USERNAME     =   "root";         // ("root") The username of the database to use as connection's credential
     const DATABASE_PASSWORD     =   "";             // ("") The password as connection's credential
     const DATABASE_NAME         =   "church";       // ("church") The database name to connects
+
+//========================================//
+
+                                     
+    #####    ##   ##### #    #  ####  
+    #    #  #  #    #   #    # #      
+    #    # #    #   #   ######  ####  
+    #####  ######   #   #    #      # 
+    #      #    #   #   #    # #    # 
+    #      #    #   #   #    #  ####   
+
+//========================================//
+
+    // Website paths
+
+    const WEBSITE_ROOT_URL = "/churchsystem";
+    const REDIRECT_PAGE_URLS = [
+        "/login/",
+        "/login/register/",
+        "/ucp/",
+        "/installer/",
+        "/ucp/user_settings/",
+        "/ucp/user_exit/",
+        "/ucp/cell_group/",
+        "/ucp/wide_view/"
+    ];
+
+    // Enumeration the paths
+
+    const URL_PAGE_LOGIN        =   0;
+    const URL_PAGE_REGISTER     =   1;
+    const URL_PAGE_UCP          =   2;
+    const URL_PAGE_INSTALLER    =   3;
+    const URL_PAGE_SETTINGS     =   4;
+    const URL_PAGE_EXIT         =   5;
+    const URL_PAGE_CELL_GROUP   =   6;
+    const URL_PAGE_WIDE_VIEW    =   7;
+
+//===================================================================================//
+                                                                               
+    #     #                                                                         
+    #     #  ####  ###### #####     #####  ###### #        ##   ##### ###### #####  
+    #     # #      #      #    #    #    # #      #       #  #    #   #      #    # 
+    #     #  ####  #####  #    #    #    # #####  #      #    #   #   #####  #    # 
+    #     #      # #      #####     #####  #      #      ######   #   #      #    # 
+    #     # #    # #      #   #     #   #  #      #      #    #   #   #      #    # 
+     #####   ####  ###### #    #    #    # ###### ###### #    #   #   ###### #####    
+
+//===================================================================================// 
+
+    // Users SESSION variables
+
+    const USER_DATA_ID              =   "UserID";
+    const USER_DATA_NAME            =   "UserName";
+    const USER_DATA_LOGGED          =   "Logged";
+    const USER_DATA_ACCOUNT_TYPE    =   "AccountType";
+    const USER_DATA_EMAIL           =   "Email";
+
+    // User Account types
+
+    const ACCOUNT_TYPE_ANY          =   0;    // Any
+    const ACCOUNT_TYPE_ADMIN        =   1;    // Pastor
+    const ACCOUNT_TYPE_LEAD_MEMBER  =   2;    // Lead of cell group
+    const ACCOUNT_TYPE_MEMBER       =   3;    // Member of cell group
+
+    // Users gender types
+
+    const GENDER_UNKNOWN            =   0; // Empty
+    const GENDER_MALE               =   1; // Male
+    const GENDER_FEMALE             =   2; // Female
+
+    // Users data database size
+
+    const MIN_USER_NAME             =   5; // 5 char as min name
+    const MIN_USER_EMAIL            =   12; // 12 char as min email
+    const MIN_USER_PASSWORD         =   4; // 4 char min as password size (password != hash)
+
+    const MAX_USER_NAME             =   64; // 64 chars as max user name
+    const MAX_USER_EMAIL            =   64; // 64 chars as max user email
+    const MAX_USER_PASSWORD         =   12; // 12 chars as max user password
 
 ?>
--- source/process/main.php
+++ source/process/main.php
@@ -8,11 +8,100 @@
 
     include "data.php";         // Including general data constants and variables
 
+    function GetUserNameById($user_id, &$dest)
+    {
+        $handle = Database_ReturnHandle();
+        $result = mysqli_query($handle, "SELECT `name` FROM `users` WHERE `userid` = '$user_id';");
+        if($result)
+        {
+            $data = mysqli_fetch_array($result);
+            $dest = $data['name'];
+            return true;
+        }
+        return false;
+    }
+
+    // Write Nav bars depending on user type account
+    function WriteHeaders()
+    {
+        $accountType = $_SESSION[USER_DATA_ACCOUNT_TYPE];
+        switch($accountType)
+        {
+            case ACCOUNT_TYPE_ANY:
+                echo '
+                        <nav>
+                            <!-- Adicione links de navegação, como opções do menu, aqui -->
+                            <ul>
+                                <li><a href="' . WEBSITE_ROOT_URL . REDIRECT_PAGE_URLS[URL_PAGE_UCP]  .'" class="link-light">Início</a></li>
+                                <li><a href="' . WEBSITE_ROOT_URL . REDIRECT_PAGE_URLS[URL_PAGE_SETTINGS]  .'" class="link-light">Configurações</a></li>
+                                <li><a href="' . WEBSITE_ROOT_URL . REDIRECT_PAGE_URLS[URL_PAGE_EXIT]  .'" class="link-light">Sair</a></li>
+                            </ul>
+                        </nav>
+                    ';
+            break;
+            case ACCOUNT_TYPE_ADMIN:
+                echo '
+                        <nav>
+                            <!-- Adicione links de navegação, como opções do menu, aqui -->
+                            <ul>
+                                <li><a href="' . WEBSITE_ROOT_URL . REDIRECT_PAGE_URLS[URL_PAGE_UCP]  .'" class="link-light">Início</a></li>
+                                <li><a href="' . WEBSITE_ROOT_URL . REDIRECT_PAGE_URLS[URL_PAGE_CELL_GROUP]  .'" class="link-light">Células</a></li>
+                                <li><a href="' . WEBSITE_ROOT_URL . REDIRECT_PAGE_URLS[URL_PAGE_WIDE_VIEW]  .'" class="link-light">Visão Geral</a></li>
+                                <li><a href="' . WEBSITE_ROOT_URL . REDIRECT_PAGE_URLS[URL_PAGE_SETTINGS]  .'" class="link-light">Configurações</a></li>
+                                <li><a href="' . WEBSITE_ROOT_URL . REDIRECT_PAGE_URLS[URL_PAGE_EXIT]  .'" class="link-light">Sair</a></li>
+                            </ul>
+                        </nav>
+                    ';
+            break;
+            case ACCOUNT_TYPE_MEMBER: 
+            case ACCOUNT_TYPE_LEAD_MEMBER:
+                echo '
+                        <nav>
+                            <!-- Adicione links de navegação, como opções do menu, aqui -->
+                            <ul>
+                                <li><a href="' . WEBSITE_ROOT_URL . REDIRECT_PAGE_URLS[URL_PAGE_UCP]  .'" class="link-light">Início</a></li>
+                                <li><a href="' . WEBSITE_ROOT_URL . REDIRECT_PAGE_URLS[URL_PAGE_CELL_GROUP]  .'" class="link-light">Célula</a></li>
+                                <li><a href="' . WEBSITE_ROOT_URL . REDIRECT_PAGE_URLS[URL_PAGE_SETTINGS]  .'" class="link-light">Configurações</a></li>
+                                <li><a href="' . WEBSITE_ROOT_URL . REDIRECT_PAGE_URLS[URL_PAGE_EXIT]  .'" class="link-light">Sair</a></li>
+                            </ul>
+                        </nav>
+                    ';
+            break;
+        }
+        
+    }
+
+
+    // Validate fields, return false if something is wrong, true if not
+    function ValidateFields($fieldname, $data)
+    {
+        switch($fieldname)
+        {
+            case "name":
+                if(strlen($data) < MIN_USER_NAME || strlen($data) > MAX_USER_NAME) return false;
+                else return true;
+                break;
+            case "email":
+                if(strlen($data) < MIN_USER_EMAIL || strlen($data) > MAX_USER_EMAIL) return false;
+                else return true;
+                break;
+            case "password":
+                if(strlen($data) < MIN_USER_PASSWORD || strlen($data) > MAX_USER_PASSWORD) return false;
+                else return true;
+                break;
+            default:
+                $erro = new Error("Invalid Fieldname passed to ValidateFields");
+                throw $erro;
+        }
+    }
+
+
     // Checks if the user is logged (and which account type he's logged in)
     function IsUserLogged($accountType = ACCOUNT_TYPE_ANY)
     {
         if(!isset($_SESSION[USER_DATA_LOGGED])) return false;
         if($_SESSION[USER_DATA_LOGGED] == true && $_SESSION[USER_DATA_ACCOUNT_TYPE] == $accountType) return true;
+        else if($_SESSION[USER_DATA_LOGGED] == true && $accountType == ACCOUNT_TYPE_ANY) return true;
         return false;
     }
 
@@ -21,6 +110,7 @@
     // Connects to the database
     function Database_Connect()
     {
+        global $DatabaseHandle;
         $DatabaseHandle = mysqli_connect(DATABASE_HOSTNAME, DATABASE_USERNAME, DATABASE_PASSWORD, DATABASE_NAME);
         if(mysqli_connect_errno())
         {
@@ -42,4 +132,20 @@
         global $DatabaseHandle;
         return mysqli_close($DatabaseHandle);
     }
+
+    
+    // Deactivated because it's unusual in the current version
+    // Returns the age by the DATE value
+    // function GetAge(string $birthday)
+    // {
+    //     $timestamp = time();
+    //     $birth_Timestamp = strtotime($birthday);
+        
+    //     $difference = ($timestamp - $birth_Timestamp);
+        
+    //     $age = floor($difference / (60 * 60 * 24 * 365.25));
+
+    //     return intval($age);
+    // }
+
 ?>
